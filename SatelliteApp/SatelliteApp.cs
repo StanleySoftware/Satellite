4580817002e8d7f3ae53edf3591a6fdac0460053
EwanStanley <ewanstanley63@gmail.com>
2021-04-22 01:17:51 +0100

    Initial commit

---
+++ SatelliteApp/SatelliteApp.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Configuration;
+
+namespace Sat
+{
+    class SatelliteApp
+    {
+        private static readonly Dictionary<string, Type> VCSTypes = new Dictionary<string, Type>()
+        {
+            { "git", typeof(SatelliteGit)}
+        };
+
+        private static void PassThrough(Stream p_instream, Stream p_outstream)
+        {
+            byte[] buffer = new byte[4096];
+            while (true)
+            {
+                int len;
+                while ((len = p_instream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    p_outstream.Write(buffer, 0, len);
+                    p_outstream.Flush();
+                }
+            }
+        }
+
+        private static void OutputReader(object p_process)
+        {
+            var process = (System.Diagnostics.Process)p_process;
+            PassThrough(process.StandardOutput.BaseStream, Console.OpenStandardOutput());
+        }
+
+        private static void ErrorReader(object p_process)
+        {
+            var process = (System.Diagnostics.Process)p_process;
+            PassThrough(process.StandardError.BaseStream, Console.OpenStandardError());
+        }
+
+        private static void InputReader(object p_process)
+        {
+            var process = (System.Diagnostics.Process)p_process;
+            PassThrough(Console.OpenStandardInput(), process.StandardInput.BaseStream);
+        }
+
+        private static int Invoke(string p_executableTarget, string p_args, string p_workingDirectory)
+        {
+            if (p_executableTarget != null)
+            {
+                // Fires up a new process to run inside this one
+                var processStartInfo = new ProcessStartInfo();
+                processStartInfo.UseShellExecute = false;
+                processStartInfo.RedirectStandardError = true;
+                processStartInfo.RedirectStandardInput = true;
+                processStartInfo.RedirectStandardOutput = true;
+                processStartInfo.FileName = p_executableTarget;
+                processStartInfo.WorkingDirectory = p_workingDirectory;
+
+                processStartInfo.Arguments = string.Join(' ', p_args);
+
+                using (System.Diagnostics.Process exeProcess = new System.Diagnostics.Process())
+                {
+                    exeProcess.StartInfo = processStartInfo;
+
+                    // Depending on your application you may either prioritize the IO or the exact opposite
+                    var outputThread = new Thread(OutputReader) { Name = "Satellite Output", Priority = ThreadPriority.Normal };
+                    var errorThread = new Thread(ErrorReader) { Name = "Satellite Error", Priority = ThreadPriority.Normal };
+                    var inputThread = new Thread(InputReader) { Name = "Satellite Input", Priority = ThreadPriority.Normal };
+
+                    // Set as background threads (will automatically stop when application ends)
+                    outputThread.IsBackground = true;
+                    errorThread.IsBackground = true;
+                    inputThread.IsBackground = true;
+
+                    // Signal to end the application
+                    ManualResetEvent stopApp = new ManualResetEvent(false);
+
+                    // Enables the exited event and set the stopApp signal on exited
+                    exeProcess.EnableRaisingEvents = true;
+                    exeProcess.Exited += (e, sender) => { stopApp.Set(); };
+
+                    try
+                    {
+                        exeProcess.Start();
+
+                        // Start the IO threads
+                        outputThread.Start(exeProcess);
+                        errorThread.Start(exeProcess);
+                        inputThread.Start(exeProcess);
+                    }
+                    catch (Exception err)
+                    {
+                        Console.Out.WriteLine($"Satellite Error: Unable to start process \'{p_executableTarget}\'. {err.Message}");
+                        Console.Out.WriteLine("Satellite: Aborting...");
+                    }
+
+                    // Wait for the child app to stop
+                    stopApp.WaitOne();
+                    return exeProcess.ExitCode;
+                }
+            }
+
+            return 0;
+        }
+
+        private static int InvokeCommand(string query, string[] args, string vcs, string targetDir)
+        {
+            //Ensure we have !invoke at the end
+            string[] keys = query.Split(':');
+            if (keys.Last() != "!invoke")
+            {
+                List<string> fullKeys = new List<string>(keys.Append("!invoke"));
+                query = String.Join(':', fullKeys);
+            }
+
+            string oldWorkingDirectory = Directory.GetCurrentDirectory();
+            string invokePrefix = Satellite.RelayGit(targetDir, query);
+            if(invokePrefix == null)
+            {
+                return -1;
+            }
+
+            string joinedArgs = String.Join(' ', args);
+            string fullCommand = $"{invokePrefix} {joinedArgs}";
+
+            string[] separatedArgs = Satellite.SeperateArgString(fullCommand);
+
+            string executableName = separatedArgs.First();
+            string fullArgs = "";
+            if (separatedArgs.Length > 1)
+            {
+                fullArgs = String.Join(' ', separatedArgs.Skip(1).Take(separatedArgs.Length - 1));
+            }
+            if (executableName != null)
+            {
+                return Invoke(executableName, fullArgs, oldWorkingDirectory);
+            }
+            return -1;
+        }
+
+        public static int Main(string[] p_args)
+        {
+            // Create a root command with some options
+            var rootCommand = new RootCommand
+            {
+                new Argument<string>(
+                    "query",
+                    description: "The query to resolve against the satellite.json files - the target for invocation."),
+                new Option<string[]>(
+                    "--args",
+                    getDefaultValue: () => new string[] {},
+                    description: "List of arguments to pass to the invoked target."),
+                new Option<string>(
+                    "--vcs",
+                    getDefaultValue: () => "git",
+                    description: $"The name for the VCS system to use. Valid values are [{String.Join(", ",VCSTypes.Keys)}]"),
+                new Option<string>(
+                    "--targetDir",
+                    getDefaultValue: () => Directory.GetCurrentDirectory(),
+                    description: "The path from which to invoke the query. By default uses the current working directory.")
+            };
+
+            rootCommand.Description = "Invoking satellite detects the root directory of the checkout containing the working directory." +
+                "It then reads a satellite.json file in that root directory and performs a lookup of the query." +
+                "It will resolve the query by traversing satellite.jsons in multiple directories if they are referenced." +
+                "Once the full query has been resolved, takes the string returned from the query, and invokes it as a process.";
+
+            // Note that the parameters of the handler method are matched according to the names of the options
+            rootCommand.Handler = CommandHandler.Create<string, string[], string, string>(InvokeCommand);
+
+            // Parse the incoming args and invoke the handler
+            return rootCommand.InvokeAsync(p_args).Result;
+        }
+    }
+}
---
+++ SatelliteLib/Satellite.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Sat
+{
+    public class Satellite
+    {
+        [DllImport("shell32.dll", SetLastError = true)]
+        static extern IntPtr CommandLineToArgvW(
+        [MarshalAs(UnmanagedType.LPWStr)] string p_cmdLine, out int p_numArgs);
+
+        private static readonly string c_satelliteFileName = "satellite.json";
+
+        private static void ReportFileStack(Stack<string> p_fileStack)
+        {
+            string fileStackReport = String.Join(Environment.NewLine, p_fileStack);
+            Console.Out.WriteLine("FileStack:");
+            Console.Out.WriteLine(fileStackReport);
+        }
+
+        public static string[] SeperateArgString(string p_args)
+        {
+            int argc;
+            var argv = CommandLineToArgvW(p_args, out argc);
+            if (argv == IntPtr.Zero)
+            {
+                throw new System.ComponentModel.Win32Exception();
+            }
+
+            var separatedArgs = new string[argc];
+
+            try
+            {
+                for (var i = 0; i < separatedArgs.Length; i++)
+                {
+                    var arg = Marshal.ReadIntPtr(argv, i * IntPtr.Size);
+                    separatedArgs[i] = Marshal.PtrToStringUni(arg);
+                }
+                return separatedArgs;
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(argv);
+            }
+        }
+
+        /// <summary>
+        /// Performs substitution of certain symbols in the terms:
+        /// - Replaces relative paths starting with './' with their absolute equivalent.
+        /// </summary>
+        /// <param name="p_term">The term in question</param>
+        /// <returns>The evaluated version of the term.</returns>
+        public static string EvaluateTerms(string p_terms)
+        {
+            string[] separatedArgs = SeperateArgString(p_terms);
+            string[] evaluatedArgs = new string[separatedArgs.Length];
+
+            for (int i = 0; i < evaluatedArgs.Length; i++)
+            {
+                string toEval = separatedArgs[i];
+                if(!Path.IsPathRooted(toEval))
+                {
+                    toEval = Path.GetFullPath(toEval);
+                }
+                evaluatedArgs[i] = toEval;
+            }
+
+            return String.Join(' ', evaluatedArgs);
+        }
+
+        private static string ResolveExpression(string[] p_keys, string p_originalExpression, Dictionary<string, string> p_currentDict, Stack<string> p_fileStack)
+        {
+            if(p_keys.Length == 0)
+            {
+                Console.Out.WriteLine($"Satellite Error: ResolveExpression called with no keys. Invalid expression: \'{p_originalExpression}\'");
+                Console.Out.WriteLine("Satellite: Aborting...");
+                return null;
+            }
+
+            //Set the working directory to the path of the json file,
+            //so that relative paths are correctly recognised.
+            string dirName = Path.GetDirectoryName(p_fileStack.Peek());
+            Directory.SetCurrentDirectory(dirName);
+
+            string value;
+            string leadingKey = p_keys.First();
+            if (p_currentDict.TryGetValue(leadingKey, out value))
+            {
+                value = EvaluateTerms(value);
+
+                string[] remainingKeys = p_keys.Skip(1).ToArray();
+                if(remainingKeys.Length > 0)
+                {
+                    //Still components of the expression left to resolve
+                    if(File.Exists(value) && Path.GetExtension(value) == ".json")
+                    {
+                        string satelliteFileContents = null;
+                        Dictionary<string, string> lookupTable = null;
+                        try
+                        {
+                            satelliteFileContents = File.ReadAllText(value);
+                        }
+                        catch (Exception err)
+                        {
+                            Console.Out.WriteLine($"Satellite Error: Reading from file \'{value}\' failed. {err.Message}");
+                            ReportFileStack(p_fileStack);
+                            Console.Out.WriteLine("Satellite: Aborting...");
+                            return null;
+                        }
+
+                        try
+                        {
+                            lookupTable = JsonSerializer.Deserialize<Dictionary<string, string>>(satelliteFileContents);
+                        }
+                        catch (Exception err)
+                        {
+                            Console.Out.WriteLine($"Satellite Error: Failed to deserialize JSON from \'{value}\'. {err.Message}");
+                            ReportFileStack(p_fileStack);
+                            Console.Out.WriteLine("Satellite: Aborting...");
+                            return null;
+                        }
+
+                        p_fileStack.Push(value);
+                        return ResolveExpression(remainingKeys, p_originalExpression, lookupTable, p_fileStack);
+                    }
+                    else
+                    {
+                        Console.Out.WriteLine($"Satellite Error: File \'{value}\' does not exist or is not a .json file.");
+                        ReportFileStack(p_fileStack);
+                        Console.Out.WriteLine("Satellite: Aborting...");
+                        return null;
+                    }
+                }
+                else
+                {
+                    return value;
+                }
+            }
+            else
+            {
+                Console.Out.WriteLine($"Satellite Error: Lookup failed for component \'{leadingKey}\' in given expression \'{p_originalExpression}\'");
+                ReportFileStack(p_fileStack);
+                Console.Out.WriteLine("Satellite: Aborting...");
+                return null;
+            }
+        }
+
+        private static string Relay(ISatellite p_satellite, string p_key)
+        {
+            //Working copy root
+            string repoRoot = p_satellite.CheckoutRoot;
+            string satelliteFile = Path.Combine(repoRoot, c_satelliteFileName);
+            string satelliteFileContents = null;
+            Dictionary<string, string> lookupTable = null;
+
+            try
+            {
+                satelliteFileContents = File.ReadAllText(satelliteFile);
+            }
+            catch (Exception err)
+            {
+                Console.Out.WriteLine("Satellite Error: Reading from \'{0}\' failed. {1}", satelliteFile, err.Message);
+                Console.Out.WriteLine("Satellite: Aborting...");
+                return null;
+            }
+
+            try
+            {
+                lookupTable = JsonSerializer.Deserialize<Dictionary<string, string>>(satelliteFileContents);
+            }
+            catch (Exception err)
+            {
+                Console.Out.WriteLine("Satellite Error: Failed to deserialize JSON from \'{0}\'. {1}", satelliteFile, err.Message);
+                Console.Out.WriteLine("Satellite: Aborting...");
+                return null;
+            }
+
+            if (lookupTable != null)
+            {
+                string[] keys = p_key.Split(':');
+                var fileStack = new Stack<string>();
+                fileStack.Push(satelliteFile);
+                return ResolveExpression(keys, p_key, lookupTable, fileStack);
+            }
+
+            return null;
+        }
+
+        public static string RelayGit(string p_originPath, string p_key)
+        {
+            string cwd = Directory.GetCurrentDirectory();
+            ISatellite satellite = new SatelliteGit(p_originPath);
+            if (!satellite.IsCheckout)
+            {
+                Console.Out.WriteLine("Satellite Error: Specified path \'{0}\' is not part of a git repository.", p_originPath);
+                Console.Out.WriteLine("Satellite: Aborting...");
+                return null;
+            }
+
+            return Relay(satellite, p_key);
+        }
+    }
+}
